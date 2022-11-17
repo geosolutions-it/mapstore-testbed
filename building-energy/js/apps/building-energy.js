40b15a7a7ca2324257ff68e864cb02e5eeb3efdd
allyoucanmap <stefano.bovio@geosolutionsgroup.com>
2022-11-17 15:06:49 +0100

    building energy app setup

---
+++ building-energy/js/apps/building-energy.js
@@ -0,0 +1,119 @@
+/*
+* Copyright 2022, GeoSolutions Sas.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+// add this binding to ensure all the streams inside components are working
+import '@mapstore/framework/libs/bindings/rxjsRecompose';
+
+import url from 'url';
+import main from '@mapstore/framework/product/main';
+import pluginsDef from '@js/plugins/def';
+import {
+    setLocalConfigurationFile,
+    setConfigProp
+} from '@mapstore/framework/utils/ConfigUtils';
+import axios from '@mapstore/framework/libs/ajax';
+import MapViewer from '@mapstore/framework/containers/MapViewer';
+import { configureMap } from '@mapstore/framework/actions/config';
+import { setControlProperty } from '@mapstore/framework/actions/controls';
+import security from '@mapstore/framework/reducers/security';
+import omit from 'lodash/omit';
+
+setLocalConfigurationFile('configs/localConfig.json');
+setConfigProp('translationsPath', ['translations', 'ms-translations']);
+setConfigProp('extensionsRegistry', 'configs/extensions.json');
+
+// list of path that need version parameter
+const pathsNeedVersion = [
+    'configs/',
+    'assets/',
+    'translations/',
+    'ms-translations/',
+    'print.json'
+];
+
+const version = __MAPSTORE_PROJECT_CONFIG__.version || 'dev';
+
+axios.interceptors.request.use(
+    config => {
+        if (config.url && version && pathsNeedVersion.filter(urlEntry => config.url.match(urlEntry))[0]) {
+            return {
+                ...config,
+                params: {
+                    ...config.params,
+                    v: version
+                }
+            };
+        }
+        return config;
+    }
+);
+
+const pages = [{
+    name: 'home',
+    path: '/',
+    component: MapViewer
+}];
+
+const MAP_TYPE = 'cesium';
+
+document.addEventListener('DOMContentLoaded', function() {
+    // example of initial security state
+    // with null this state is not initialized
+    const user = true;
+    const securityState = user && {
+        security: {
+            user: user,
+            token: '' // this token is applied to the request defined in the localConfig authenticationRules properties
+        }
+    };
+    // this is an example of dynamic map loading via query param
+    // there are other possibilities such use injected map data in the page
+    // or use the internal react routing
+    // the important steps is to populate the configureMap action with a valid map config
+    const params = url.parse(window.location.href, true).query || {};
+    const mapName = params.map || 'map';
+    // load a base map configuration
+    axios.get(`configs/${mapName}.json`)
+        .then(({ data }) => {
+            // initialize the mapstore app
+            main(
+                {
+                    targetId: 'container',
+                    pages,
+                    initialState: {
+                        defaultState: {
+                            ...securityState,
+                            maptype: {
+                                mapType: MAP_TYPE,
+                                last2dMapType: MAP_TYPE
+                            }
+                        }
+                    },
+                    appReducers: {
+                        security
+                    },
+                    appEpics: {},
+                    printingEnabled: false
+                },
+                pluginsDef,
+                // TODO: use default main import to avoid override
+                (cfg) => ({
+                    ...cfg,
+                    // remove epics that manage the map type for the standard product
+                    appEpics: omit(cfg.appEpics, [
+                        'syncMapType',
+                        'updateLast2dMapTypeOnChangeEvents',
+                        'restore2DMapTypeOnLocationChange'
+                    ]),
+                    initialActions: [
+                        setControlProperty.bind(null, 'toolbar', 'expanded', false),
+                        configureMap.bind(null, data, 1, true)
+                    ]
+                }));
+        });
+});
---
+++ building-energy/js/plugins/def.js
@@ -0,0 +1,92 @@
+
+/**
+* Copyright 2022, GeoSolutions Sas.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
+import AboutPlugin from '@mapstore/framework/product/plugins/About';
+import AddGroupPlugin from '@mapstore/framework/plugins/AddGroup';
+import AnnotationsPlugin from '@mapstore/framework/plugins/Annotations';
+import BackgroundSelectorPlugin from '@mapstore/framework/plugins/BackgroundSelector';
+import CookiePlugin from '@mapstore/framework/plugins/Cookie';
+import DrawerMenuPlugin from '@mapstore/framework/plugins/DrawerMenu';
+import FeatureEditorPlugin from '@mapstore/framework/plugins/FeatureEditor';
+import FilterLayerPlugin from '@mapstore/framework/plugins/FilterLayer';
+import FullScreenPlugin from '@mapstore/framework/plugins/FullScreen';
+import IdentifyPlugin from '@mapstore/framework/plugins/Identify';
+import LayerDownloadPlugin from '@mapstore/framework/plugins/LayerDownload';
+import LocatePlugin from '@mapstore/framework/plugins/Locate';
+import MapPlugin from '@mapstore/framework/plugins/Map';
+import MapExportPlugin from '@mapstore/framework/plugins/MapExport';
+import MapFooterPlugin from '@mapstore/framework/plugins/MapFooter';
+import MapImportPlugin from '@mapstore/framework/plugins/MapImport';
+import MapViewsPlugin from '@mapstore/framework/plugins/MapViews';
+import MeasurePlugin from '@mapstore/framework/plugins/Measure';
+import MetadataExplorerPlugin from '@mapstore/framework/plugins/MetadataExplorer';
+import MousePositionPlugin from '@mapstore/framework/plugins/MousePosition';
+import NotificationsPlugin from '@mapstore/framework/plugins/Notifications';
+import OmniBarPlugin from '@mapstore/framework/plugins/OmniBar';
+import QueryPanelPlugin from '@mapstore/framework/plugins/QueryPanel';
+import SearchPlugin from '@mapstore/framework/plugins/Search';
+import SearchByBookmarkPlugin from '@mapstore/framework/plugins/SearchByBookmark';
+import SearchServicesConfigPlugin from '@mapstore/framework/plugins/SearchServicesConfig';
+import SettingsPlugin from '@mapstore/framework/plugins/Settings';
+import SharePlugin from '@mapstore/framework/plugins/Share';
+import SidebarMenuPlugin from '@mapstore/framework/plugins/SidebarMenu';
+import SwipePlugin from '@mapstore/framework/plugins/Swipe';
+import TOCPlugin from '@mapstore/framework/plugins/TOC';
+import TOCItemsSettingsPlugin from '@mapstore/framework/plugins/TOCItemsSettings';
+import TimelinePlugin from '@mapstore/framework/plugins/Timeline';
+import ToolbarPlugin from '@mapstore/framework/plugins/Toolbar';
+import WidgetsPlugin from '@mapstore/framework/plugins/Widgets';
+import WidgetsBuilderPlugin from '@mapstore/framework/plugins/WidgetsBuilder';
+import WidgetsTrayPlugin from '@mapstore/framework/plugins/WidgetsTray';
+import ZoomAllPlugin from '@mapstore/framework/plugins/ZoomAll';
+
+export default {
+    plugins: {
+        // framework plugins
+        AboutPlugin,
+        AddGroupPlugin,
+        AnnotationsPlugin,
+        BackgroundSelectorPlugin,
+        CookiePlugin,
+        DrawerMenuPlugin,
+        FeatureEditorPlugin,
+        FilterLayerPlugin,
+        FullScreenPlugin,
+        IdentifyPlugin,
+        LayerDownloadPlugin,
+        LocatePlugin,
+        MapPlugin,
+        MapExportPlugin,
+        MapFooterPlugin,
+        MapImportPlugin,
+        MapViewsPlugin,
+        MeasurePlugin,
+        MetadataExplorerPlugin,
+        MousePositionPlugin,
+        NotificationsPlugin,
+        OmniBarPlugin,
+        QueryPanelPlugin,
+        SearchPlugin,
+        SearchByBookmarkPlugin,
+        SearchServicesConfigPlugin,
+        SettingsPlugin,
+        SharePlugin,
+        SidebarMenuPlugin,
+        SwipePlugin,
+        TOCPlugin,
+        TOCItemsSettingsPlugin,
+        TimelinePlugin,
+        ToolbarPlugin,
+        WidgetsPlugin,
+        WidgetsBuilderPlugin,
+        WidgetsTrayPlugin,
+        ZoomAllPlugin
+    },
+    requires: {}
+};
